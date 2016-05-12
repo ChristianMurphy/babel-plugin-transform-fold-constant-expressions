77893d7549283179916de5d651a89daff5752a65
Christian Murphy <christian.murphy.42@gmail.com>
2016-05-12 14:13:04 -0700

    passing unit test

--- test/index.js
+++ test/index.js
@@ -1,10 +1,10 @@
 import test from 'ava';
-import babel from 'babel-core';
+import {transform} from 'babel-core';
 import plugin from '../dist/index';
 
 test('constant unchanged', t => {
-  const example = 'var a = 1';
-  const expected = 'var a = 1';
-  const actual = babel.transform(example, {plugins: [plugin]});
+  const example = 'var a = 1;';
+  const expected = 'var a = 1;';
+  const actual = transform(example, {plugins: [plugin]}).code;
   t.is(actual, expected);
 });
