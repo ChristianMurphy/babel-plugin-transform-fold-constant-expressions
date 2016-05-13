4356de1839a94104ca22b58e59a0bb27569b526f
Christian Murphy <christian.murphy.42@gmail.com>
2016-05-13 10:15:52 -0700

    number expression tests

--- test/index.js
+++ test/index.js
@@ -2,13 +2,6 @@ import test from 'ava';
 import {transform} from 'babel-core';
 import plugin from '../dist/index';
 
-test('constant number unchanged', t => {
-  const example = 'var a = 1;';
-  const expected = 'var a = 1;';
-  const actual = transform(example, {plugins: [plugin]}).code;
-  t.is(actual, expected);
-});
-
 test('constant boolean unchanged', t => {
   const example = 'var a = true;';
   const expected = 'var a = true;';
@@ -54,6 +47,55 @@ test('false boolean mixed expression reduced', t => {
 test('boolean expression with variable partially reduced', t => {
   const example = 'var a = b || true && false;';
   const expected = 'var a = b || false;';
+  const actual = transform(example, {plugins: [plugin]}).code;
+  t.is(actual, expected);
+});
+
+test('constant number unchanged', t => {
+  const example = 'var a = 1;';
+  const expected = 'var a = 1;';
+  const actual = transform(example, {plugins: [plugin]}).code;
+  t.is(actual, expected);
+});
+
+test('number addition expression reduced', t => {
+  const example = 'var a = 1 + 1;';
+  const expected = 'var a = 2;';
+  const actual = transform(example, {plugins: [plugin]}).code;
+  t.is(actual, expected);
+});
+
+test('number subtraction expression reduced', t => {
+  const example = 'var a = 1 - 1;';
+  const expected = 'var a = 0;';
+  const actual = transform(example, {plugins: [plugin]}).code;
+  t.is(actual, expected);
+});
+
+test('number multiplication expression reduced', t => {
+  const example = 'var a = 2 * 2;';
+  const expected = 'var a = 4;';
+  const actual = transform(example, {plugins: [plugin]}).code;
+  t.is(actual, expected);
+});
+
+test('number division expression reduced', t => {
+  const example = 'var a = 2 / 2;';
+  const expected = 'var a = 1;';
+  const actual = transform(example, {plugins: [plugin]}).code;
+  t.is(actual, expected);
+});
+
+test('number mixed expression reduced', t => {
+  const example = 'var a = 2 * 2 + 2 * 2;';
+  const expected = 'var a = 8;';
+  const actual = transform(example, {plugins: [plugin]}).code;
+  t.is(actual, expected);
+});
+
+test('number expression with variable partially reduced', t => {
+  const example = 'var a = b + 2 * 2;';
+  const expected = 'var a = b + 4;';
   const actual = transform(example, {plugins: [plugin]}).code;
   t.is(actual, expected);
 });
