d7f42f839afee30ba2cc4bdc5e66b9ae6b966212
Christian Murphy <christian.murphy.42@gmail.com>
2016-05-13 10:28:13 -0700

    add doc comments

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 export default function({types: t}) {
+  /**
+   * Visitor for expressions looks if children are literal values and the same
+   * type then reduces expressions when possible.
+   * @param {Object} path - an AST expression node
+   */
   function visit(path) {
     let replacement = null;
     if (t.isNumericLiteral(path.node.left) && t.isNumericLiteral(path.node.right)) {
@@ -12,6 +17,11 @@ export default function({types: t}) {
     }
   }
 
+  /**
+   * Evaluates an AST expression
+   * @param {Object} path - an AST node with an operator, a left, and right child
+   * @return {any} literal value of the expression
+   */
   function evaluate(path) {
     return eval(`${path.node.left.value} ${path.node.operator} ${path.node.right.value}`);
   }
