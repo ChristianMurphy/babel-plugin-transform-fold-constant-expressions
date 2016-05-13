ef03c6c1469c6ed07674c5a4b811cb66984dc5b5
Christian Murphy <christian.murphy.42@gmail.com>
2016-05-13 10:01:07 -0700

    boolean tests

--- test/index.js
+++ test/index.js
@@ -2,9 +2,58 @@ import test from 'ava';
 import {transform} from 'babel-core';
 import plugin from '../dist/index';
 
-test('constant unchanged', t => {
+test('constant number unchanged', t => {
   const example = 'var a = 1;';
   const expected = 'var a = 1;';
+  const actual = transform(example, {plugins: [plugin]}).code;
+  t.is(actual, expected);
+});
+
+test('constant boolean unchanged', t => {
+  const example = 'var a = true;';
+  const expected = 'var a = true;';
+  const actual = transform(example, {plugins: [plugin]}).code;
+  t.is(actual, expected);
+});
+
+test('true boolean and expression reduced', t => {
+  const example = 'var a = true && true;';
+  const expected = 'var a = true;';
+  const actual = transform(example, {plugins: [plugin]}).code;
+  t.is(actual, expected);
+});
+
+test('true boolean or expression reduced', t => {
+  const example = 'var a = false || true;';
+  const expected = 'var a = true;';
+  const actual = transform(example, {plugins: [plugin]}).code;
+  t.is(actual, expected);
+});
+
+test('false boolean or expression reduced', t => {
+  const example = 'var a = false || false;';
+  const expected = 'var a = false;';
+  const actual = transform(example, {plugins: [plugin]}).code;
+  t.is(actual, expected);
+});
+
+test('true boolean mixed expression reduced', t => {
+  const example = 'var a = true && false || true;';
+  const expected = 'var a = true;';
+  const actual = transform(example, {plugins: [plugin]}).code;
+  t.is(actual, expected);
+});
+
+test('false boolean mixed expression reduced', t => {
+  const example = 'var a = true && (false || false);';
+  const expected = 'var a = false;';
+  const actual = transform(example, {plugins: [plugin]}).code;
+  t.is(actual, expected);
+});
+
+test('boolean expression with variable partially reduced', t => {
+  const example = 'var a = b || true && false;';
+  const expected = 'var a = b || false;';
   const actual = transform(example, {plugins: [plugin]}).code;
   t.is(actual, expected);
 });
