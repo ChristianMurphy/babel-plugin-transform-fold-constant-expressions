3f0c47314b36c38e4f9bf1f47dee5413b1d62eda
Christian Murphy <christian.murphy.42@gmail.com>
2016-05-20 09:30:19 -0700

    initial tests for library call and inferred functionality

--- test/index.js
+++ test/index.js
@@ -44,6 +44,13 @@ test('false boolean mixed expression reduced', t => {
   t.is(actual, expected);
 });
 
+test('inferred boolean expression reduced', t => {
+  const example = 'var a = true; var b = false || a;';
+  const expected = 'var a = true;var b = true;';
+  const actual = transform(example, {plugins: [plugin]}).code;
+  t.is(actual, expected);
+});
+
 test('boolean expression with variable partially reduced', t => {
   const example = 'var a = b || true && false;';
   const expected = 'var a = b || false;';
@@ -93,6 +100,27 @@ test('number mixed expression reduced', t => {
   t.is(actual, expected);
 });
 
+test('inferred number expression reduced', t => {
+  const example = 'var a = 2 + 2; var b = a * 2;';
+  const expected = 'var a = 4;var b = 8;';
+  const actual = transform(example, {plugins: [plugin]}).code;
+  t.is(actual, expected);
+});
+
+test('number library calls reduced', t => {
+  const example = 'var a = Math.pow(2, 3);';
+  const expected = 'var a = 8;';
+  const actual = transform(example, {plugins: [plugin]}).code;
+  t.is(actual, expected);
+});
+
+test('number library Math.random calls not reduced', t => {
+  const example = 'var a = Math.random();';
+  const expected = 'var a = Math.random();';
+  const actual = transform(example, {plugins: [plugin]}).code;
+  t.is(actual, expected);
+});
+
 test('number expression with variable partially reduced', t => {
   const example = 'var a = b + 2 * 2;';
   const expected = 'var a = b + 4;';
@@ -124,6 +152,20 @@ test('single quoted string concat expression reduced', t => {
 test('double quoted string concat expression reduced', t => {
   const example = 'var a = "a" + "a";';
   const expected = 'var a = "aa";';
+  const actual = transform(example, {plugins: [plugin]}).code;
+  t.is(actual, expected);
+});
+
+test('inferred string concat expression reduced', t => {
+  const example = 'var a = "a"; var b = a + "a";';
+  const expected = 'var a = "a";var b = "aa";';
+  const actual = transform(example, {plugins: [plugin]}).code;
+  t.is(actual, expected);
+});
+
+test('string libary calls  reduced', t => {
+  const example = 'var a = "  a  ".trim();';
+  const expected = 'var a = "a";';
   const actual = transform(example, {plugins: [plugin]}).code;
   t.is(actual, expected);
 });
